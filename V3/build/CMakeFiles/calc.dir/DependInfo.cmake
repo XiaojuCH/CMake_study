8990774b278b233afd6d1bd2e61a4cf6b6ee2bcf
XiaojuCH <3022484748@qq.com>
2025-12-02 19:31:50 +0800

    保存了v2的文件，即制作动态和静态库。新建v3文件夹，用来调用库文件

---
+++ V3/build/CMakeFiles/calc.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/cmake_study/CMake_study/V2/src/add.cpp" "CMakeFiles/calc.dir/src/add.cpp.o" "gcc" "CMakeFiles/calc.dir/src/add.cpp.o.d"
+  "/root/cmake_study/CMake_study/V2/src/div.cpp" "CMakeFiles/calc.dir/src/div.cpp.o" "gcc" "CMakeFiles/calc.dir/src/div.cpp.o.d"
+  "/root/cmake_study/CMake_study/V2/src/mult.cpp" "CMakeFiles/calc.dir/src/mult.cpp.o" "gcc" "CMakeFiles/calc.dir/src/mult.cpp.o.d"
+  "/root/cmake_study/CMake_study/V2/src/sub.cpp" "CMakeFiles/calc.dir/src/sub.cpp.o" "gcc" "CMakeFiles/calc.dir/src/sub.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ V3/build/CMakeFiles/calc.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/root/cmake_study/CMake_study/V2/abcdefg/libcalc.a"
+)
